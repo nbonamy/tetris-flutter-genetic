d825609e7239c5393c323b41956c33efd46cfc7e
Nicolas Bonamy <nicolas@bonamy.fr>
2020-05-18 08:12:42 -0500

    show row# on board

--- lib/widgets/board_widget.dart
+++ lib/widgets/board_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:stanley/stanley.dart';
 import 'package:tetris/model/game.dart';
+import 'package:tetris/utils/ui.dart';
 import 'package:tetris/widgets/block_painter.dart';
 
 class BoardWidget extends StatelessWidget {
@@ -23,21 +24,44 @@ class BoardWidget extends StatelessWidget {
     double height = MediaQuery.of(context).size.height * 0.6;
     double cellSize = min(width / game.board.width, height / game.board.height);
 
-    return Decorator(
-      width: game.board.width * cellSize + 2 * borderSize,
-      height: game.board.height * cellSize + 1 * borderSize,
-      borderLeft: borderSize,
-      borderRight: borderSize,
-      borderBottom: borderSize,
-      borderColor: Colors.red,
-      child: CustomPaint(
-        foregroundPainter: BlockPainter(
-          blocks: game.getBoardState(true),
-          currentPiece: game.currentPiece,
-          cellSize: cellSize,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: _buildRowIds(cellSize),
         ),
-      ),
+        Decorator(
+          marginLeft: 16,
+          width: game.board.width * cellSize + 2 * borderSize,
+          height: game.board.height * cellSize + 1 * borderSize,
+          borderLeft: borderSize,
+          borderRight: borderSize,
+          borderBottom: borderSize,
+          borderColor: Colors.red,
+          child: CustomPaint(
+            foregroundPainter: BlockPainter(
+              blocks: game.getBoardState(true),
+              currentPiece: game.currentPiece,
+              cellSize: cellSize,
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  _buildRowIds(cellSize) {
+
+    List<Widget> ids = List();
+    for (int i=game.board.height-1; i>=0; i--) {
+      ids.add(Decorator(
+        height: cellSize,
+        child: UIUtils.text('${i+1}', size: 12),
+      ));
+    }
+    return ids;
+
   }
 
 }
