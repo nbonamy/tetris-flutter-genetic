f213bd2c02bf7086440c87207dea08985ac5768f
Nicolas Bonamy <nicolas@bonamy.fr>
2020-05-17 21:46:31 -0500

    status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:tetris/screens/game.dart';
 
 void main() => runApp(TetrisApp());
 
 class TetrisApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Colors.transparent);
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
     return MaterialApp(
       title: 'Tetris',
       theme: ThemeData(
