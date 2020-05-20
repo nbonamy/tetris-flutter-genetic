f561e6c26cb3262c869d9f3a3e0b9ce946147978
Nicolas Bonamy <nicolas@bonamy.fr>
2020-05-19 22:36:02 -0500

    new font

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class Consts {
 
-  static const String kFontFamily = 'PixelMix';
+  static const String kFontFamily = 'Joystix';
   static const Color kTextColor = Colors.white;
 
 }
